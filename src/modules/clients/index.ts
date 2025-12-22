2667f3a6c38f7ea77c52f6c88886a3160fd4f815
fabianofontes-oss <fabianofontes@gmail.com>
2025-12-22 01:44:33 -0300

    auto-backup: 2025-12-22 01:44

---
+++ src/modules/clients/index.ts
@@ -0,0 +1,27 @@
+/**
+ * Módulo de Clients
+ * 
+ * Re-exports de actions e types
+ */
+
+// Re-export actions
+export {
+  listClientsAction,
+  getClientAction,
+  createClientAction,
+  updateClientAction,
+  deleteClientAction,
+  searchClientsAction,
+  getClientStatsAction,
+} from './actions';
+
+// Re-export types
+export type {
+  Client,
+  CreateClientInput,
+  UpdateClientInput,
+  ClientFilters,
+  PaginatedClients,
+  ClientStats,
+} from './actions';
+
