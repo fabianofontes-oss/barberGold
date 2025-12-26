f12a942b73b5cd2cbf7a41eda0b1e409d9e58b2a
fabianofontes-oss <fabianofontes@gmail.com>
2025-12-26 19:01:41 -0300

    auto-backup: 2025-12-26 19:01

---
+++ src/app/app/barber-club/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { ClubDashboard } from '@/modules/barber-club/components/ClubDashboard';
+
+export default function BarberClubPage() {
+  return <ClubDashboard />;
+}
---
+++ src/app/app/catalog/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { Catalog } from '@/modules/catalog/Catalog';
+
+export default function CatalogPage() {
+  return <Catalog />;
+}
